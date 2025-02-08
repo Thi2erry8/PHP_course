907eb36f721108236014920b73044075c28ed5ec
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-02-07 20:57:51 -0500

    variables

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo'Hello World'; ?></title>
 </head>
 <body>
-       <? echo'Hello Word' ?>
+      
+       <?php 
+       $name= 'Jonathan';
+       
+       echo'Bonjour  '.$name; 
+       
+       
+       ?>
+       
 </body>
 </html>
