9a35bdff78b2a12d5aae431f06270e93e23f184e
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-03-05 17:36:38 -0500

    cleaning

--- inc/function.php
+++ inc/function.php
@@ -1,36 +1,3 @@
 <?php
-      function authentification_user($email, $password) {
-        return $email == USER_NAME && $password == PASSWORD;
-      }
       
-      function redirect($url){
-        header("Location: $url");
-      }
-
-      function is_user_authentificated(){
-        return isset($_SESSION['email']);
-      }
-
-      function ensure_user_is_authentificated(){
-        if(!is_user_authentificated()){
-            redirect('login.php');
-            die();
-        }
-      }
-
-      function get_data(){
-        $fname = CONFIG['data_file'];
-        
-        $json = '';
-        
-        if(!file_exists($fname)){
-            file_put_contents($fname,'');
-        }else{
-            $json = file_get_contents($fname);
-        }
-
-        return $json;
-
-
-      }
 ?>
